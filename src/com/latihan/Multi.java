8ceca798b46906f78cbbc5a708e4c90702895647
herdianset <herdyansetiawan@gmail.com>
2021-10-31 22:20:32 +0800

    multidimensi

---
+++ src/com/latihan/Multi.java
@@ -0,0 +1,20 @@
+package com.latihan;
+
+public class Multi {
+    public static void main(String[] args) {
+        int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
+        //int x = myNumbers[1][2];
+        //System.out.println(x);
+        
+        //contoh lain
+        for(int i = 0; i < myNumbers.length; i++)
+        {
+            for(int j = 0; j < myNumbers[i].length; j++)
+            {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
+
+    }
+
+}
